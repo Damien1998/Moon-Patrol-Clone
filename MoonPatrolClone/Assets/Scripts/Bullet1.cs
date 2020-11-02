840b0c1be46505ee8a68675dbe7dce9cd3717460
Damien1998 <damianbombala98@gmail.com>
2020-11-02 23:22:48 +0100

    Samochodzik jeździ i szczela

---
+++ MoonPatrolClone/Assets/Scripts/Bullet1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet1 : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public int damage = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Rock rock = hitInfo.GetComponent<Rock>();
+        if (rock != null)
+        {
+            rock.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+
+}
